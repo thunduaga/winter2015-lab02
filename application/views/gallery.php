f876e0aceb4d40592871fc1595cf4b061270b656
thunduaga <chriswumbojumbo@msn.com>
2015-01-15 17:12:17 -0800

    CQ I THINK I DID IT FINALLY had to move js and img folders back to the root
    I GET MY WEEKEND
    5:12pm
    Signed-off-by:thunduaga <chriswumbojumbo@msn.com>

--- application/views/gallery.php
+++ application/views/gallery.php
@@ -2,5 +2,5 @@
             
                 {thetable}
                 <h1>The image gallery, complete :)</h1>
-                <p> The images above are the three most recently posted.</p>
+                <p> The images above will be shown in a lightbox HA!.</p>
             
