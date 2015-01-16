ac580ff99a917396773c955d909d36cf4f092589
thunduaga <chriswumbojumbo@msn.com>
2015-01-15 16:20:31 -0800

    CQ got database working 4:20pm Signed-off-by:thunduaga <chriswumbojumbo@msn.com>

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -96,7 +96,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 
-$autoload['libraries'] = array('parser');//'database',
+$autoload['libraries'] = array('parser', 'database');//'database',
 
 
 /*
@@ -173,7 +173,7 @@ $autoload['language'] = array();
 |	$autoload['model'] = array('first_model' => 'first');
 */
 
-$autoload['model'] = array();
+$autoload['model'] = array('images');
 
 
 /* End of file autoload.php */
